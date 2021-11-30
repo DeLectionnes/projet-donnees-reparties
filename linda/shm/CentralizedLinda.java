a6cb2fdae2b2e91f216600397b520465a8afb625
Monville letu Raphaelle <rmonvill@verlaine.enseeiht.fr>
2021-11-30 17:33:36 +0100

    Début CentralizedLinda

--- linda/shm/CentralizedLinda.java
+++ linda/shm/CentralizedLinda.java
@@ -1,5 +1,8 @@
 package linda.shm;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
 import linda.Callback;
 import linda.Linda;
 import linda.Tuple;
@@ -7,9 +10,38 @@ import linda.Tuple;
 /** Shared memory implementation of Linda. */
 public class CentralizedLinda implements Linda {
 	
+	private boolean writer; 
+	private int nbLecters;
+	private int nbTake;
+	
+	private Lock monitor; 
+	
+	private Condition LPossible;
+	private Condition WPossible;
+	private Condition TPossible;
+	
+	private ArrayList<Tuple> TSpaces; 
+	
     public CentralizedLinda() {
+    	this.monitor = new java.util.concurrent.locks.ReentrantLock();
+    	
+    	this.writer = false; 
+    	this.nbLecteur = 0;
+    	this.nbTake = 0;
+    	
+    	this.EPossible = monitor.newCondition();
+    	this.LPossible = monitor.newCondition();
+    	this.TPossible = monitor.newCondition();
+    	
+    	this.TSpaces = new ArrayList<Tuple>();
     }
 
-    // TO BE COMPLETED
+    public void write(Tuple t) {
+    	if(!((nbLecters==0)&&(((ReentrantLock) this.monitor).getWaitQueueLength(this.LPossible) == 0)&& !writer) ) {
+    		WPossible.await();
+    	}
+    	writer = true;
+    	TSpaces.add(t);
+    }
 
 }
